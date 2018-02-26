eb0e397b8ce810c6d0227b65a69f8c0a254bbaa8
Paul Klimov <klimov.paul@gmail.com>
2018-02-27 00:17:42 +0200

    basic infrastructure created

---
+++ tests/TestCase.php
@@ -0,0 +1,98 @@
+<?php
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
+namespace yiiunit\rest;
+
+use yii\di\Container;
+use yii\helpers\ArrayHelper;
+use Yii;
+
+/**
+ * This is the base class for all unit tests.
+ */
+abstract class TestCase extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * Clean up after test.
+     * By default the application created with [[mockApplication]] will be destroyed.
+     */
+    protected function tearDown()
+    {
+        parent::tearDown();
+        $this->destroyApplication();
+    }
+
+    /**
+     * Populates Yii::$app with a new application
+     * The application will be destroyed on tearDown() automatically.
+     * @param array $config The application configuration, if needed
+     * @param string $appClass name of the application class to create
+     */
+    protected function mockApplication($config = [], $appClass = \yii\console\Application::class)
+    {
+        new $appClass(ArrayHelper::merge([
+            'id' => 'testapp',
+            'basePath' => __DIR__,
+            'vendorPath' => dirname(__DIR__) . '/vendor',
+        ], $config));
+    }
+
+    protected function mockWebApplication($config = [], $appClass = \yii\web\Application::class)
+    {
+        new $appClass(ArrayHelper::merge([
+            'id' => 'testapp',
+            'basePath' => __DIR__,
+            'vendorPath' => dirname(__DIR__) . '/vendor',
+            'components' => [
+                'request' => [
+                    'cookieValidationKey' => 'wefJDF8sfdsfSDefwqdxj9oq',
+                    'scriptFile' => __DIR__ .'/index.php',
+                    'scriptUrl' => '/index.php',
+                ],
+            ]
+        ], $config));
+    }
+
+    /**
+     * Destroys application in Yii::$app by setting it to null.
+     */
+    protected function destroyApplication()
+    {
+        Yii::$app = null;
+        Yii::$container = new Container();
+    }
+
+    /**
+     * Asserting two strings equality ignoring line endings
+     *
+     * @param string $expected
+     * @param string $actual
+     */
+    public function assertEqualsWithoutLE($expected, $actual)
+    {
+        $expected = str_replace(["\r", "\n"], '', $expected);
+        $actual = str_replace(["\r", "\n"], '', $actual);
+        $this->assertEquals($expected, $actual);
+    }
+
+    /**
+     * Invokes object method, even if it is private or protected.
+     * @param object $object object.
+     * @param string $method method name.
+     * @param array $args method arguments
+     * @return mixed method result
+     */
+    protected function invoke($object, $method, array $args = [])
+    {
+        $classReflection = new \ReflectionClass(get_class($object));
+        $methodReflection = $classReflection->getMethod($method);
+        $methodReflection->setAccessible(true);
+        $result = $methodReflection->invokeArgs($object, $args);
+        $methodReflection->setAccessible(false);
+        return $result;
+    }
+}
