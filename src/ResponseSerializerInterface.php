70205c0af00ded5a617d8cabf1201e3615d31711
Dmitriy Derepko <xepozz@list.ru>
2020-03-21 14:10:05 +0300

    Added TODO

--- src/ResponseSerializerInterface.php
+++ src/ResponseSerializerInterface.php
@@ -6,5 +6,6 @@ use Psr\Http\Message\ResponseInterface;
 
 interface ResponseSerializerInterface
 {
+    // TODO may be $code is redundant?
     public function serialize(int $code, $data): ResponseInterface;
 }
