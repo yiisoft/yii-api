82e4f2886848a7e6601cc766bd27ea7ec3da6684
Dmitriy Derepko <xepozz@list.ru>
2020-03-21 13:42:20 +0300

    Added PaginationHeaderTag middleware with tests

---
+++ src/Middleware/PaginationHeaderTag.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Yiisoft\Yii\Rest\Middleware;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+final class PaginationHeaderTag implements MiddlewareInterface
+{
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        $response = $handler->handle($request);
+
+        $links = [];
+        $paginationLinks = /*$pagination->getLinks() ?? */[];
+        foreach ($paginationLinks as $rel => $url) {
+            $links[] = "<$url>; rel=$rel";
+        }
+
+        return $response
+            ->withHeader('Link', implode(', ', $links))
+            ->withHeader('X-Pagination-Total-Count', 0)
+            ->withHeader('X-Pagination-Page-Count', 0)
+            ->withHeader('X-Pagination-Current-Page', 0)
+            ->withHeader('X-Pagination-Per-Page', 0);
+    }
+}
