dac27547a7172493593d9110967f259305cf0ad6
Rustam Mamadaminov <rmamdaminov@gmail.com>
2020-05-11 04:12:50 +0500

    Fix exceptions (#54)

--- src/RestControllerTrait.php
+++ src/RestControllerTrait.php
@@ -8,41 +8,41 @@ trait RestControllerTrait
 {
     public function delete(): ResponseInterface
     {
-        throw new \Exception('Method is not implemented yet');
+        throw new \LogicException('Method is not implemented yet');
     }
 
     public function head(): ResponseInterface
     {
-        throw new \Exception('Method is not implemented yet');
+        throw new \LogicException('Method is not implemented yet');
     }
 
     public function get(): ResponseInterface
     {
-        throw new \Exception('Method is not implemented yet');
+        throw new \LogicException('Method is not implemented yet');
     }
 
     public function list(): ResponseInterface
     {
-        throw new \Exception('Method is not implemented yet');
+        throw new \LogicException('Method is not implemented yet');
     }
 
     public function options(): ResponseInterface
     {
-        throw new \Exception('Method is not implemented yet');
+        throw new \LogicException('Method is not implemented yet');
     }
 
     public function patch(): ResponseInterface
     {
-        throw new \Exception('Method is not implemented yet');
+        throw new \LogicException('Method is not implemented yet');
     }
 
     public function post(): ResponseInterface
     {
-        throw new \Exception('Method is not implemented yet');
+        throw new \LogicException('Method is not implemented yet');
     }
 
     public function put(): ResponseInterface
     {
-        throw new \Exception('Method is not implemented yet');
+        throw new \LogicException('Method is not implemented yet');
     }
 }
